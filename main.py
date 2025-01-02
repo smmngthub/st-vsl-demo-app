bdc9c8d3fdd6fcb18f86c77a8a286c9d21d3439d
smmngthub <sulthancais@gmail.com>
2025-01-02 11:33:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,5 @@ import streamlit as st
 # from user_login import st_user_login 
 
 
-st.subheader("Bismillahirrahumanirrahim")
 
-# app_user = st_user_login()
-# st.write(" Ret Value", app_user)
-"""
-if app_user == "VSL":
-  st.toast("Hi you are in Vessel Application")
-  # Build the sidebar
-  # st.sidebar.subheader("Vessels")
-  st.divider()
-  # Build the Main Navigations  
-elif app_user == "TBT":
-  
-  st.toast("Hi you are in TBT Application")
-  st.balloons()
-  
-  # Build the sidebar
-  # with st.sidebar:
-  #  pass
-    
-  # Build the Main Navigations
-else:
-  # st.toast("Somehting wrong Happened")
-  st.error("Somehting wrong Happened")
-"""
   
