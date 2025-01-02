beda161c7c69615d9297a943e9320037f67de4a4
smmngthub <sulthancais@gmail.com>
2025-01-02 10:39:14 +0530

    Update user_login.py

--- user_login.py
+++ user_login.py
@@ -5,8 +5,18 @@ def st_user_login():
 
   try: 
     user_name = st.text_input(label="User Name", value="Admin", placeholder="Please Enter Your Login", max_chars=20)
+    user_pwd = st.text_input(label="Password", placeholder="Please enter your passwod", max_chars=25)
 
-    st.snow()
+    submitted = st.button("Login")
+    
+    # trimmed_user_name = user_name.trim()
+    # trimmed_user_pwd = user_pwd.trim() 
+    if len(user_name) > 1 and user_name = "Vessel123" and user_pwd = "Hey@123"
+      st.snow()
+      st.success("You have successfully logged in to the system")
+    elif:
+      st.error("Error, entering user name and/or password. Please contact the Administrator")
+    
   except:
     st.error(f"Error encountered in logging in {e}")
   
