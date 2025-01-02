8f79efef62ca946010b48ad157a0556dd4038c7f
smmngthub <sulthancais@gmail.com>
2025-01-02 12:03:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ if __name__ == '__main__':
   st.write(" Ret Value", app_user)
 # st.switch_page("entry_screen.py")
 
+  pages = {
+      "Your account": [
+          st.Page("create_account.py", title="Create your account"),
+          st.Page("manage_account.py", title="Manage your account"),
+      ],
+      "Resources": [
+          st.Page("learn.py", title="Learn about us"),
+          st.Page("trial.py", title="Try it out"),
+      ],
+  }
+  
+  pg = st.navigation(pages)
+  pg.run()
