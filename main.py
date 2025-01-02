d4811b71bc6cef1c8e16f454aea290267e7b3558
smmngthub <sulthancais@gmail.com>
2025-01-02 11:22:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ app_user = st_user_login()
 if app_user == "VSL":
   st.toast("Hi you are in Vessel Application")
   # Build the sidebar
-  with st.sidebar:
-    st.subheader = "Vessels"
-    st.divider()
+  st.sidebar.subheader = "Vessels"
+  st.divider()
   # Build the Main Navigations  
 elif app_user == "TBT":
   
