bf307caf9ec4bbb12272821fdf189ebf833a38be
smmngthub <sulthancais@gmail.com>
2025-01-02 12:16:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
   app_user = st_user_login()
   st.write(" Ret Value", app_user)
   # st.switch_page("entry_screen.py")
-  if app_user == "VSL":
-    st.switch_page(st.Page("entry_screen.py"))
+  # if app_user == "VSL":
+  st.switch_page(st.Page("entry_screen.py"))
