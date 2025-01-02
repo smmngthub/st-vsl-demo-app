53b3a981f4002111c8d297bd17e355a8d2d8867b
smmngthub <sulthancais@gmail.com>
2025-01-02 10:55:50 +0530

    Update user_login.py

--- user_login.py
+++ user_login.py
@@ -5,7 +5,10 @@ def st_user_login():
 
   try: 
     user_name = st.text_input(label="User Name", value="", placeholder="Please Enter Your Login", max_chars=20)
-    user_pwd = st.text_input(label="Password", type="password", placeholder="Please enter your passwod", max_chars=25)
+    user_pwd = st.text_input(label="Password", type="password", 
+                             placeholder="Please enter your passwod", 
+                             help="Please enter your password",
+                             max_chars=25)
 
     submitted = st.button("Login")
     
@@ -13,12 +16,17 @@ def st_user_login():
     # trimmed_user_pwd = user_pwd.trim() 
     # if len(user_name) > 1 and user_name = "Vessel123" and user_pwd = "Hey@123"
     if submitted: 
-      if user_name == "Vessel123" and user_pwd == "Hey@123":  
+      if user_name == "Vessel123" and user_pwd == "Vessel@123":  
         st.snow()
         st.success("You have successfully logged in to the system")
+        go_to_app = "VSL" 
+      elif user_name == "Tbt123" and user_pwd == "Tbt@123":
+        st.balloon()
+        st.success("You have successfully logged in to the system")
+        go_to_app = "TBT"
       else:
         st.error("Error, entering user name and/or password. Please contact the Administrator")
-    return submitted
+    return go_to_app
   except:
     st.error(f"Error encountered in logging in {e}")
     st.stop()
