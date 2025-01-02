d4eb2d8e59b65f635b6a4770a5c2b75134c828d3
smmngthub <sulthancais@gmail.com>
2025-01-02 13:15:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from user_login import st_user_login
 
 st.write(" Bismillahirrahumanirrahim")
   
-st.header("Testing...")
+#st.header("Testing...")
 st.title("Bismillah")
 
-st.write("Test" , __name__)
+# st.write("Test" , __name__)
 if __name__ == '__main__':
   app_user = st_user_login()
-  st.write(" Ret Value", app_user)
+  # st.write(" Ret Value", app_user)
   # pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
   # pg.run()  
 
