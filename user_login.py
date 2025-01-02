a66824f0ff372e4d660d7f54ba9789a97649fa74
smmngthub <sulthancais@gmail.com>
2025-01-02 10:32:35 +0530

    Update user_login.py

--- user_login.py
+++ user_login.py
@@ -4,9 +4,9 @@ import streamlit as st
 def st_user_login():
 
   try: 
-    user_name = st.text_input(label="User Name", Value="Admin", placeholder="Please Enter Your Login", max_chars=20)
+    user_name = st.text_input(label="User Name", value="Admin", placeholder="Please Enter Your Login", max_chars=20)
 
     st.snow()
   except:
-    st.error("Error encountered in logging in")
+    st.error(f"Error encountered in logging in {e}")
   
