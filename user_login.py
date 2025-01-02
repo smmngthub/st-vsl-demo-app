2a418c12c1980f8a7e044e96536c99bc6e6187fd
smmngthub <sulthancais@gmail.com>
2025-01-02 10:27:05 +0530

    Update user_login.py

--- user_login.py
+++ user_login.py
@@ -1,1 +1,12 @@
 # Bismillahirrahumanirrahim
+import streamlit as st
+
+def st_user_login()
+
+  try: 
+    user_name = st.text_input(label="User Name", Value="Admin", placeholder="Please Enter Your Login", max_chars=20)
+
+    st.snow()
+  except:
+    st.error("Error encountered in logging in")
+  
