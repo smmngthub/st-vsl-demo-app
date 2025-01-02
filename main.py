ac8ac9e337022ec7a3429ecbf0b5c95700cfe272
smmngthub <sulthancais@gmail.com>
2025-01-02 11:10:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ from user_login import st_user_login
 
 st.subheader("Bismillahirrahumanirrahim")
 
-st_user_login()
+app_user = st_user_login()
+if app_user == "VSL":
+  st.toast("Hi you are in Vessel Application")
+  # Build the sidebar
+  
+  # Build the Main Navigations  
+elif app_user == "TBT":
+  st.toast("Hi you are in TBT Application")
+  # Build the sidebar
+  # Build the Main Navigations
+elif:
+  st.toast("Somehting wrong Happened")
+  st.error("Somehting wrong Happened")
 
+  
