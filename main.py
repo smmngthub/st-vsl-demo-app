0d8f10fbc2e18c095b64a9f60f1d8d7a42193c62
smmngthub <sulthancais@gmail.com>
2025-01-02 11:59:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ st.write("Test" , __name__)
 if __name__ == '__main__':
   app_user = st_user_login()
   st.write(" Ret Value", app_user)
-st.switch_page("./entry_screen.py")
+st.switch_page("entry_screen.py")
 
