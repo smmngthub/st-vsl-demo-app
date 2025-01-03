3a2a9d537f66f8dc347b91b64b5bc1ce29b27fe9
smmngthub <sulthancais@gmail.com>
2025-01-03 09:06:43 +0530

    Update Vessel.py

--- Master/Vessel.py
+++ Master/Vessel.py
@@ -1,1 +1,12 @@
+# Bismillahirrahumanirrahim
+import streamlit as st
 
+st.title("You are in Vessel Master Screen")
+
+# check if the user is logged-in AND have the permission to this screen
+
+# List down all the fields, capture and store it in sqlite3 database
+
+# have at least two tabs (Listing of 'All Vessels' (with Edit & Delete option)  & 'Adding New Vessel')
+
+# 
