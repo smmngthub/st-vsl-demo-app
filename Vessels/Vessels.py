75061b5ad1d7301438482fc9df7d1546101d868d
smmngthub <sulthancais@gmail.com>
2025-01-03 20:46:15 +0530

    Update Vessels.py

--- Vessels/Vessels.py
+++ Vessels/Vessels.py
@@ -6,3 +6,40 @@ st.title(":green[You are in the Vessels Page]")
 
 # New Vessels & List of Existing Vessels
 
+
+Vessel Number = st.text_input("")
+Vessel Name = st.text_input("")
+Vessel Category = st.text_input("")
+Registration Number = st.text_input("")
+Model = st.text_input("")
+Nautical Reading = st.text_input("")
+Nautical Miles = st.text_input("")
+Status = st.text_input("")
+Available = st.text_input("")
+Flag = st.text_input("")
+Port of Registry = st.text_input("")
+Passanger Capacity = st.text_input("")
+Chassis Number = st.text_input("")
+Manufacture = st.text_input("")
+Last Departure Date  = st.text_input("")
+Last Service Reading  = st.text_input("")
+Last Service Date  = st.text_input("")
+Registration Exp Date  = st.text_input("")
+Site/Project  = st.text_input("")
+Business Unit  = st.text_input("")
+Vessel Value  = st.text_input("")
+Purchase Date  = st.text_input("")
+Warranty Date  = st.text_input("")
+Required Geo Location while Inspection
+Assign Inspection Form  = st.text_input("")
+
+
+Operator Name  = st.text_input("Operator Name")
+Maintenance Priority  = st.text_input("Maintenance Priority")
+Normal  = st.text_input("Normal")
+Ownership Mode  = st.text_input("Ownership Mode")
+Vessel Note  = st.text_input("Vessel Note")
+Vessel Image  = st.text_input("Vessel Image")
+GPS EnabledDevice ID  = st.text_input("GPS EnabledDevice ID")
+GPS Vessel ID  = st.text_input("GPS Vessel ID")
+
