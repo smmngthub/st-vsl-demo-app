d232eda2ba87c24789914bae13bfe96c79865e00
smmngthub <sulthancais@gmail.com>
2025-01-03 22:08:24 +0530

    Update dashboard.py

--- reports/dashboard.py
+++ reports/dashboard.py
@@ -5,7 +5,11 @@ import numpy as np
 # import matplotlib.pyplot as plt
 
 st.title("Dash Board")
-
+title_writing = "Dash Board"
+title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #808080; font-size: 40px; ' \
+               f'font-weight: bold;">{title_writing}</p>'
+st.markdown(title_format, unsafe_allow_html=True)
 
 a, b = st.columns(2)
 c, d = st.columns(2)
