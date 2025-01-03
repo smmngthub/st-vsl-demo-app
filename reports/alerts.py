8870d423a07915c1e9ed90c0bf72c1501acb8966
smmngthub <sulthancais@gmail.com>
2025-01-03 22:19:15 +0530

    Update alerts.py

--- reports/alerts.py
+++ reports/alerts.py
@@ -2,6 +2,12 @@
 
 import streamlit as st
 
+title_writing = "Weather Forecast in Port"
+title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #808080; font-size: 40px; ' \
+               f'font-weight: bold;">{title_writing}</p>'
+st.markdown(title_format, unsafe_allow_html=True)
+
 a, b = st.columns(2)
 c, d = st.columns(2)
 
