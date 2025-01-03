9371f8328be37d933feff5ba2fe5894307eefc2c
smmngthub <sulthancais@gmail.com>
2025-01-03 14:36:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import time
 
 st.logo("logo/sample_logo_image.jpg", icon_image="logo/sample_logo_image_1.jpg")
-
+# side_bar_nav = st.get_option('client.showSidebarNavigation')
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
     st.toast('Welcome Guest!', icon='😍')
