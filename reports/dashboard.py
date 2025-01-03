97f5a2c3444137c683936d8861dfddb794e47c4a
smmngthub <sulthancais@gmail.com>
2025-01-03 22:06:42 +0530

    Update dashboard.py

--- reports/dashboard.py
+++ reports/dashboard.py
@@ -1,6 +1,8 @@
 # Bismillahirrahumanirrahim
 
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.title("Dash Board")
 
@@ -14,4 +16,15 @@ b.metric("Wind", "4 mph", "2 mph", border=True)
 c.metric("Humidity", "77%", "5%", border=True)
 d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
 
+st.title("Interactive Data Visualization")
+
+# Slider for selecting a number
+number = st.slider("Select a number", 1, 100)
+
+# Generate random data
+data = np.random.randn(number)
+
+# Create a histogram
+plt.hist(data, bins=20)
+st.pyplot(plt)
 
