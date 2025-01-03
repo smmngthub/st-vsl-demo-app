1459cb3de62be60ed9b890810a69515883785a62
smmngthub <sulthancais@gmail.com>
2025-01-03 19:37:24 +0530

    Update img_related.py

--- RandD/img_related.py
+++ RandD/img_related.py
@@ -1,5 +1,25 @@
 # Bismillahirrahumanirrahim
 
 import streamlit as st
+from PIL import Image
+import os
 
-st.title(":red[You are in Image Related R & D]")
+st.title(":blue[You are in Image Related R & D]") # :red
+
+def load_image(image_file):
+  img = Image.open(image_file)
+  return img
+
+image_file = st.file_uploader("Upload an Image", type=["png", "jpeg", "jpeg"])
+if image_file is not None:
+  file_details = {"File Name": image_file.name,
+                 "File Type": image_file.type,
+                 "File Size": image_file.size,
+                 "File ID": image_file.file_id}
+  st.write(file_details)
+  img = load_image(image_file)
+  st.image(img, use_container_width=150)
+
+  with open(os.path.join("images", image_file.name), "wb") as f:
+    f.write(image_file.getbuffer())
+  st.success("file Saved")
