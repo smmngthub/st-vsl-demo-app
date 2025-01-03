768a8452c967fde0383be630992351c38eb7c341
smmngthub <sulthancais@gmail.com>
2025-01-03 20:09:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,18 +19,17 @@ def login():
                              max_chars=25)
 
     if st.button("Log in"):
-
         if user_name == "Vessel123" and user_pwd == "Vessel@123":  
             st.snow()
             st.success("You have successfully logged in to the system")
             go_to_app = "VSL"
             # st.switch_page("entry_screen.py")
-          elif user_name == "Tbt123" and user_pwd == "Tbt@123":
+        elif user_name == "Tbt123" and user_pwd == "Tbt@123":
             st.balloon()
             st.success("You have successfully logged in to the system")
             go_to_app = "TBT"
             # st.switch_page("entry_screen.py")
-          else:
+        else:
             st.error("Error, entering user name and/or password. Please contact the Administrator")
             st.stop()
               
