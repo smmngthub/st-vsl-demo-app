44236e7039a22befca573978c91942acda82fa71
smmngthub <sulthancais@gmail.com>
2025-01-03 22:24:20 +0530

    Update createUsers.py

--- admin/createUsers.py
+++ admin/createUsers.py
@@ -1,1 +1,20 @@
+# Bismillahirrahumanirrahim
+import streamlit as st
 
+st.title(":Orange[User Creation]")
+def create_new_users():
+
+  with st.form(key="user_creation")
+    user_name = st.text_input("Useer Name")
+    st.write("USer Name", user_name)
+    
+  
+  return True
+
+user_action = st.radio("Action", ["New User", "Existing"], horizontal=True)
+if user_action == "New User":
+  # call the form to enter the New Users
+  create_new_users()
+  pass
+elif user_action == "Existing":
+  pass
