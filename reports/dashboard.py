da8bece3c4e9846896188631b9b01bf02016ea2f
smmngthub <sulthancais@gmail.com>
2025-01-03 22:03:34 +0530

    Update dashboard.py

--- reports/dashboard.py
+++ reports/dashboard.py
@@ -2,5 +2,16 @@
 
 import streamlit as st
 
-st.title("Bismillahirrahumanirrahim")
+st.title("Dash Board")
+
+
+a, b = st.columns(2)
+c, d = st.columns(2)
+
+a.metric("Temperature", "30°F", "-9°F", border=True)
+b.metric("Wind", "4 mph", "2 mph", border=True)
+
+c.metric("Humidity", "77%", "5%", border=True)
+d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
+
 
