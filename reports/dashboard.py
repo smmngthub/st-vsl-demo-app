ef944d29548a80182426b17b0dd6eed00418007b
smmngthub <sulthancais@gmail.com>
2025-01-03 22:16:11 +0530

    Update dashboard.py

--- reports/dashboard.py
+++ reports/dashboard.py
@@ -11,16 +11,6 @@ title_format = f'<p style="text-align: center; font-family: ' \
                f'font-weight: bold;">{title_writing}</p>'
 st.markdown(title_format, unsafe_allow_html=True)
 
-a, b = st.columns(2)
-c, d = st.columns(2)
-
-a.metric("Temperature", "30°F", "-9°F", border=True)
-b.metric("Wind", "4 mph", "2 mph", border=True)
-
-c.metric("Humidity", "77%", "5%", border=True)
-d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
-
-
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
 with st.expander("See explanation"):
