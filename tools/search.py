b6dafd42455cc938e18b6a3371125ee0420cc695
smmngthub <sulthancais@gmail.com>
2025-01-04 08:13:26 +0530

    Update search.py

--- tools/search.py
+++ tools/search.py
@@ -8,7 +8,13 @@ def load_data(data):
 st.title("Adavance Search Section")
 
 vessels = load_data("data_files/Vessels.csv")
+world_cities = load_data("data_files/worldcities.csv")
 
 with st.expander("Vessels View"):
   st.dataframe(vessels, hide_index=True)
+
+with st.expander("Cities View"):
+  st.dataframe(world_cities, hide_index=True)
+
+
 
