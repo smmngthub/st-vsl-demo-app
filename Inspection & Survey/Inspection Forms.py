ca18d706ce3bc14c82f02d1296cf30b488fb96eb
smmngthub <sulthancais@gmail.com>
2025-01-04 10:32:03 +0530

    Update Inspection Forms.py

--- Inspection & Survey/Inspection Forms.py
+++ Inspection & Survey/Inspection Forms.py
@@ -1,11 +1,21 @@
 # Bismillahirrahumanirrahim
 
 import streamlit as st
-st.title("You are in the Inspection Forms Page")
+#st.title("You are in the Inspection Forms Page")
+title_writing = "Inspection Form"
+title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #808080; font-size: 40px; ' \
+               f'font-weight: bold;">{title_writing}</p>'
+st.markdown(title_format, unsafe_allow_html=True)
 
+# st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+# st.subheader("This is a subheader with a divider", divider="gray")
+# st.subheader("These subheaders have rotating dividers", divider=True)
+
+st.subheader("_Add Inspection Item_ :blue[cool] :sunglasses:", divider=True)
 
 def collect_form_details():
-  st.write("Add Inspection Item")
+  # st.write("Add Inspection Item")
   st.write("---")
   inspect_item = st.text_input("Inspection Item")
   rad_type = st.radio("Type", ["Good-Repair-Replace-NA", "Yes-No-NA", "Text Field", "Number", "Pass-Fail-NA", "Ok-Fault-NA"], horizontal=True)
