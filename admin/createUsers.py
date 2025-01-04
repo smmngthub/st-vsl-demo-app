a50689624cc4d5b5a6fe27c6bccf92ee33cbf760
smmngthub <sulthancais@gmail.com>
2025-01-04 05:34:57 +0530

    Update createUsers.py

--- admin/createUsers.py
+++ admin/createUsers.py
@@ -1,13 +1,16 @@
 # Bismillahirrahumanirrahim
 import streamlit as st
 
-st.title(":Orange[User Creation]")
+st.title(":orange[User Creation]")
 def create_new_users():
 
   # with st.form(key="user_creation")
-  user_name = st.text_input("Useer Name")
-  st.write("USer Name", user_name)
-    
+  
+  user_name = st.text_input("User Name")
+  st.write("User Name", user_name)
+  pwd = st.text_input("You Password", type=password, placeholder="Please enter your password")
+  re-pwd = st.text_input("Re-Enter Password", type=password, placeholder="Please re-enter your password")
+  btn_register = st.button("Register")
   
   return True
 
@@ -18,3 +21,10 @@ if user_action == "New User":
   pass
 elif user_action == "Existing":
   pass
+
+def toggle_and_text()
+  cols = st.columns(2)
+  cols[0].toggle("Toggle") 
+  cols[1].text_area("Enter Text")
+  
+toggle_and_text()
