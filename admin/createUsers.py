2cac7ccdca9b3d3ea7de16c1f8db32861fb11365
smmngthub <sulthancais@gmail.com>
2025-01-04 05:37:43 +0530

    Update createUsers.py

--- admin/createUsers.py
+++ admin/createUsers.py
@@ -1,15 +1,15 @@
 # Bismillahirrahumanirrahim
 import streamlit as st
 
-st.title(":orange[User Creation]")
+st.title(":green[User Creation]")
 def create_new_users():
 
   # with st.form(key="user_creation")
   
   user_name = st.text_input("User Name")
   st.write("User Name", user_name)
-  pwd = st.text_input("You Password", type=password, placeholder="Please enter your password")
-  re-pwd = st.text_input("Re-Enter Password", type=password, placeholder="Please re-enter your password")
+  pwd = st.text_input("You Password", type="password", placeholder="Please enter your password")
+  re-pwd = st.text_input("Re-Enter Password", type="password", placeholder="Please re-enter your password")
   btn_register = st.button("Register")
   
   return True
