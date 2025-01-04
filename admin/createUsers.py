5c670c61152bb15420e1d9305110ad31c6c8a319
smmngthub <sulthancais@gmail.com>
2025-01-04 06:18:00 +0530

    Update createUsers.py

--- admin/createUsers.py
+++ admin/createUsers.py
@@ -27,16 +27,16 @@ if user_action == "New User":
     pic_type = st.radio("Pic Mode", ["Upload", "Camera"], horizontal=True)
     st.write("Picture Type: ", pic_type)
     if pic_type == "Upload":
-      uploaded_photo = st.file_uploader("Upload a Photo", type=["png", "jpeg", "jpg"])
-      # pass
+      # uploaded_photo = st.file_uploader("Upload a Photo", type=["png", "jpeg", "jpg"])
+      pass
     elif pic_type == "Camera":
-      camera_photo = st.camera_input("Take a Photo"")
-      if camera_photo is not None:
-        st.write(" Hi Camera Photo")
-      # pass
+      #camera_photo = st.camera_input("Take a Photo"")
+      #if camera_photo is not None:
+        #st.write(" Hi Camera Photo")
+      pass
     # toggle_and_pic()
   
-  pass
+  # pass
 elif user_action == "Existing":
   pass
 
