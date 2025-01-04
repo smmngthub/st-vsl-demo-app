c957df10540ec7f08eaf4cf7497ffa9f107d0d0a
smmngthub <sulthancais@gmail.com>
2025-01-04 08:24:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import time
 
-st.logo("logo/sample_logo_image.jpg", icon_image="logo/sample_logo_image_1.jpg")
+st.logo("logo/horizontal_blue.png", icon_image="logo/icon_blue.png")
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
