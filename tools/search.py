997ac9a4843d66ab07e0cf94f9ad45e903debbe9
smmngthub <sulthancais@gmail.com>
2025-01-04 07:45:56 +0530

    Update search.py

--- tools/search.py
+++ tools/search.py
@@ -1,1 +1,13 @@
+# Bismillahirrahumanirrahim
+import streamlit as st
+import pandas as pd
+
+def load_data():
+  pd.read_csv(data)
+
+st.title("Adavance Search Section")
+
+world = load_data("data_files/Vessels.csv")
+
+
 
