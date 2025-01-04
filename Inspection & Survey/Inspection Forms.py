a9257126db35ff22e6c4f1f9dc7e9e0e4cf8447e
smmngthub <sulthancais@gmail.com>
2025-01-04 10:24:49 +0530

    Update Inspection Forms.py

--- Inspection & Survey/Inspection Forms.py
+++ Inspection & Survey/Inspection Forms.py
@@ -6,7 +6,10 @@ st.title("You are in the Inspection Forms Page")
 
 def collect_form_details():
   st.write("Add Inspection Item")
+
+  inspect_item = st.text_input("Inspection Item")
   
+
   save = st.button("Save")
   if save:
     st.success(" Successfully submitted the information")
