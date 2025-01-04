1dffa9a9294bfd6dcbc500e13ca68f88cacee463
smmngthub <sulthancais@gmail.com>
2025-01-04 10:29:09 +0530

    Update Inspection Forms.py

--- Inspection & Survey/Inspection Forms.py
+++ Inspection & Survey/Inspection Forms.py
@@ -6,7 +6,7 @@ st.title("You are in the Inspection Forms Page")
 
 def collect_form_details():
   st.write("Add Inspection Item")
-
+  st.write("---")
   inspect_item = st.text_input("Inspection Item")
   rad_type = st.radio("Type", ["Good-Repair-Replace-NA", "Yes-No-NA", "Text Field", "Number", "Pass-Fail-NA", "Ok-Fault-NA"], horizontal=True)
   reqd = st.checkbox("Required")
