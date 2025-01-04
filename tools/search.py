81568f5b5a313ff1ac85d05134dae44336d1e3f0
smmngthub <sulthancais@gmail.com>
2025-01-04 08:17:44 +0530

    Update search.py

--- tools/search.py
+++ tools/search.py
@@ -8,13 +8,20 @@ def load_data(data):
 st.title("Adavance Search Section")
 
 vessels = load_data("data_files/Vessels.csv")
-world_cities = load_data("data_files/worldcities.csv")
+world = load_data("data_files/worldcities.csv")
 
 with st.expander("Vessels View"):
   st.dataframe(vessels, hide_index=True)
 
 with st.expander("Cities View"):
-  st.dataframe(world_cities, hide_index=True)
+  st.dataframe(world, hide_index=True)
 
+cities_list = world["city"].unique().tolist()
+
+selected_city = st.multiselect("Cities", cities_list, default=["Chennai"])
+
+with st.expander("Filtered View"):
+  df = world[world["city"].isin(selected_city)]
+  st.dataframe(df)
 
 
